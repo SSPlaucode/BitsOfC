92c77edd8834db6aa8397c91ae6b640986a8811b
Shubham Singh Panwar <127646743+SSPlaucode@users.noreply.github.com>
2025-07-22 09:39:16 +0530

    Add files via upload

---
+++ temperature.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //TEMPERATURE CONVERSION CHOICE
+
+    char choice = '\0';
+    float fahrenheit = 0.0f;
+    float celsius = 0.0f;
+
+    printf("Temperature Conversion Program");
+    printf("C. Celsius to Fahrenheit\n");
+    printf("F. Fahrenheit to Celsius\n");
+    printf("Is the temp in Celsius (C) or Fahrenheit (F)?: ");
+    scanf("%c", &choice);
+
+    if(choice == 'C')
+    {
+        printf("Enter the temperatue in Celsius: ");
+        scanf("%f", &celsius);
+        fahrenheit = (celsius * 9 / 5) + 32; //C to F
+        printf("%.1f Celsisus is equal to %.1f Fahrenheit", celsius, fahrenheit);
+    }
+    else if(choice == 'F')
+    {
+        printf("Enter the temperatue in Fahrenheit: ");
+        scanf("%f", &fahrenheit);
+        celsius = (fahrenheit - 32) * 5 / 9;
+        printf("%.1f Fahrenheit is equal to %.1f Celsius", fahrenheit, celsius);
+    }
+    else
+    {
+        printf("Invalid choice! Please select C or F\n");
+    }
+
+    return 0;
+}
